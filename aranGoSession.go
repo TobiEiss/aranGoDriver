8932114b0efd337f20479b9b53228dafa4ca26a6
tobi <eiss.tobias@googlemail.com>
2016-12-11 14:06:26 +0100

    use models to marshal and unmarshal json

--- aranGoSession.go
+++ aranGoSession.go
@@ -6,8 +6,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"encoding/json"
+
+	"github.com/TobiEiss/aranGoDriver/models"
 )
 
+// AranGoSession represent to Session
 type AranGoSession struct {
 	urlRoot   string
 	jwtString string
@@ -15,19 +20,32 @@ type AranGoSession struct {
 
 const urlAuth = "/_open/auth"
 
+// NewAranGoDriverSession creates a new instance of a AranGoDriver-Session.
+// Need a host (e.g. "http://localhost:8529/")
 func NewAranGoDriverSession(host string) *AranGoSession {
 	return &AranGoSession{host, ""}
 }
 
+// Connect to arangoDB
 func (session AranGoSession) Connect(username string, password string) {
-	resp := post(&session, urlAuth, "{\"username\":\""+username+"\", \"password\":\""+password+"\"}")
-	fmt.Println(resp)
+	credentials := models.Credentials{}
+	credentials.Username = username
+	credentials.Password = password
+
+	resp := post(&session, urlAuth, credentials)
+	session.jwtString = resp["jwt"].(string)
 }
 
-func post(session *AranGoSession, url string, jsonBody string) string {
+func post(session *AranGoSession, url string, object interface{}) map[string]interface{} {
+	// marshal body
+	jsonBody, err := json.Marshal(object)
+	failOnError(err, "Cant marshal object")
+
+	// build url
 	url = session.urlRoot + url
 	fmt.Println("URL:>", url)
 
+	// build request
 	var jsonString = []byte(jsonBody)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonString))
 	req.Header.Set("Content-Type", "application/json")
@@ -39,7 +57,11 @@ func post(session *AranGoSession, url string, jsonBody string) string {
 
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-	return string(body)
+
+	// unmarshal to map
+	var responseMap map[string]interface{}
+	err = json.Unmarshal(body, &responseMap)
+	return responseMap
 }
 
 func failOnError(err error, msg string) {
