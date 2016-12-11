c1e04c9930aa46f60d8aeac8115d8ef5c7742913
tobi <eiss.tobias@googlemail.com>
2016-12-11 18:44:48 +0100

    extend tests

--- aranGoDriver_test.go
+++ aranGoDriver_test.go
@@ -28,7 +28,24 @@ func TestMain(t *testing.T) {
 	}
 
 	session.Connect("root", "ILoBhREd36LB8USwpcHcCz4hLjj8k")
-	t.Log(session.ListDBs())
+	list := session.ListDBs()
+	assertTrue(!contains(list, "testDB"))
+	t.Log(list)
 	session.CreateDB("testDB")
 	t.Log(session.ListDBs())
+}
+
+func assertTrue(test bool) {
+	if !test {
+		panic("Assertion failed")
+	}
+}
+
+func contains(stringList []string, searchString string) bool {
+	for _, value := range stringList {
+		if value == searchString {
+			return true
+		}
+	}
+	return false
 }
