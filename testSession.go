f32ec9a1fcd77d3a4dd91961e8601c0654cec6dd
tobi <eiss.tobias@googlemail.com>
2016-12-11 15:50:52 +0100

    fit testsession

--- testSession.go
+++ testSession.go
@@ -1,16 +1,23 @@
 package aranGoDriver
 
+import (
+	"fmt"
+)
+
 type TestSession struct {
+	database []string
 }
 
 func NewTestSession() *TestSession {
 	return &TestSession{}
 }
 
+// Connect test
 func (session TestSession) Connect(username string, password string) {
-
+	fmt.Println("Connect to DB")
 }
 
-func (session TestSession) CreateDB(dbname string) {
-
+// CreateDB test create a db
+func (session *TestSession) CreateDB(dbname string) {
+	session.database = append(session.database, dbname)
 }
