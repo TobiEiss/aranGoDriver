dc6d656e758c778f213664a407ee7533e8172625
tobi <eiss.tobias@googlemail.com>
2016-12-27 17:36:17 +0100

    arangoDB-ident ignore fields while marshal/unmarshal

--- models/arangoID.go
+++ models/arangoID.go
@@ -3,8 +3,8 @@ package models
 type (
 	// ArangoID for arangoDB-Identification
 	ArangoID struct {
-		ID  string `json:"_id"`
-		Key string `json:"_key"`
-		Rev string `json:"_rev"`
+		ID  string `json:"_id,omitempty"`
+		Key string `json:"_key,omitempty"`
+		Rev string `json:"_rev,omitempty"`
 	}
 )
